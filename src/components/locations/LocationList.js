fc3250feeed496e58d0aad596a748165dc5ff44c
Zach Dugger <ztd522@gmail.com>
2022-03-02 11:29:03 -0500

    implementing useEffect function

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react"
+
+export const LocationList = () => {
+    return (
+        <h1>Locations</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { KandyKorner } from './components/KandyKorner';
 import reportWebVitals from './reportWebVitals';
+import { LocationList } from './components/locations/LocationList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <KandyKorner />
+    <KandyKorner />,
+    <LocationList />
   </React.StrictMode>,
   document.getElementById('root')
 );
